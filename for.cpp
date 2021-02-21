9362b12f47521652049969342f69d53e8afc34ba
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-02-21 12:06:26 +0200

    Create for.cpp

---
+++ for.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int luku, i;
+	
+	cout << "Anna kokonaisluku:";
+	cin >> luku;
+  
+	for (i = 1; i <= luku; i++) {
+		cout << i << endl;
+	}
+	return 0;
+}
